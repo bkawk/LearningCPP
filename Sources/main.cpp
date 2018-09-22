7e8dac0f05e5ba7969da257d2752fd6bde11bd4c
Will in China <email.will.in.china@gmail.com>
2018-09-23 00:37:35 +0800

    variables

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, world!\n";
+    char name[5] = "Will";
+    char initial = 'C';
+    int age = 43;
+    bool happy = true;
+    float score = 8.9;
+    
+    cout 
+    << name << endl 
+    << initial << endl 
+    << age << endl 
+    << happy << endl 
+    << score << endl;
 }
