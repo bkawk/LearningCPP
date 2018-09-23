a0b80b747c844b6bf6c8c9c30bfbb12ad3085c3a
Will in China <email.will.in.china@gmail.com>
2018-09-23 12:54:03 +0800

    arrays

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -53,5 +53,31 @@ int main() {
         cin >> age;
     } while (age != 43);
 
+    // for loop, in this example counter is only available within the loop as its initialised as part of the for loop
+    for (int counter = 1; counter <= 10; counter++) {
+        cout << "You just got " << counter << " wishes" << endl;
+    };
+    // arrays must contain the same types you cant mix
+    // arrays can be limited in length
+    int lights[6] = {1,2,3,4,5,6};
+    cout << lights[3] << endl;
+
+    // arrays can be unlimited 
+    int fish[] = {1,2,3,4,5,6,7,8,9,10};
+    cout << fish[0] << endl;
+
+    // arrays can be asigned after they are initialised if the aray has a length
+    int dogs[5];
+    dogs[0] = 1;
+    dogs[1] = 2;
+    cout << dogs[0] << endl;
+
+    // two dimensional arrays. rows / columns 
+    int studentScores[2][5] = {
+        {1,2,3,4,5},
+        {6,7,8,9,10}
+    };
+    cout << studentScores[0][2] << endl;
+
     return 0;
 }
